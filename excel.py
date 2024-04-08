0fe1d2a0fc7313fa2cb74d3198b3b7b15b18b0a2
F4s0lix <janwawrzyniakszkola@gmail.com>
2024-04-08 20:32:32 +0200

    dziala

--- excel.py
+++ excel.py
@@ -1,29 +1,16 @@
+import csv
 import pandas as pd
 import os
 import shutil
 from openpyxl.styles import PatternFill, Border, Side
-from datetime import datetime
 
 class ExcelHandler:
-    def __init__(self, excel_filepath: str) -> None:
+    def __init__(self, excel_filepath: str = 'dane/03_27_2024_10_52_07-RB 2024 01 (3).xlsx', output_path: str = 'output.xlsx') -> None:
         if not os.path.exists(excel_filepath):
             raise FileNotFoundError(f'Nie mozna znalexc pliku: {excel_filepath}')
         self.excel_filepath = excel_filepath
         self.csv_dir = 'csv_data/'
-        self.polish_months_names = {
-                    1: "Styczeń",
-                    2: "Luty",
-                    3: "Marzec",
-                    4: "Kwiecień",
-                    5: "Maj",
-                    6: "Czerwiec",
-                    7: "Lipiec",
-                    8: "Sierpień",
-                    9: "Wrzesień",
-                    10: "Październik",
-                    11: "Listopad",
-                    12: "Grudzień",
-                }
+        self.output_path = output_path
 
         self.CELL_COLOR = PatternFill(start_color='FFFF00', end_color='FFFF00', fill_type='solid')
         self.BORDER = Side(style='thin', color='000000')
@@ -55,40 +42,62 @@ class ExcelHandler:
     
     def create_excel_file(self, output_filename: str = 'output.xlsx') -> None:
         with pd.ExcelWriter(output_filename, engine='openpyxl') as writer:
+            stan_form = '='
             for file in os.listdir(self.csv_dir):
                 filename = file.removesuffix('.csv')
+                stan_form += f"'{filename}'.D2+"
                 data = pd.read_csv(f'{self.csv_dir}{file}', sep=';')
                 data.to_excel(writer, sheet_name=filename, index=False)
 
                 sheet = writer.sheets[filename]
-                sheet.cell(1, 4, 'SUMA') # description of value below
+                sheet.cell(1, 4, 'STAN') # description of value below
                 sheet.cell(1, 4).fill = self.CELL_COLOR
                 sheet.cell(1, 4).border = self.BORDER_STYLE
                 end_cell = len(data['description']) + 2
                 sheet.cell(2, 4, f'=SUM(B2:B{end_cell})-SUM(C2:C{end_cell})')
                 sheet.cell(2, 4).fill = self.CELL_COLOR
+            sheet = writer.book.create_sheet('STAN')
+            sheet.cell(1, 1, 'STAN')
+            sheet.cell(1, 1).fill = self.CELL_COLOR
+            #stan_form = stan_form.removesuffix('+')
+            sheet.cell(1, 2, stan_form)
+            sheet.cell(1, 3, 'usun "+" z komorki obok, zeby zaczelo dzialac')
 
-    def write_data_to_one_excel_file(self, output_filename: str = 'output.xlsx') -> None:
-        if not os.path.exists(output_filename):
-            self.create_excel_file(output_filename)
-            return
-        with pd.ExcelFile(output_filename, engine='openpyxl') as writer:
-            for file in os.listdir(self.csv_dir):
-                filename = file.removesuffix('.csv')
-                excel_data = pd.read_excel(writer, sheet_name=filename)
-                data_length = len(excel_data['description'])
-                print(filename, data_length)
-                sheet = writer.sheets[filename]
-                month = self.polish_months_names[datetime.now().month]
-                sheet.cell(1, data_length, month)
-                sheet.cell(1, data_length).fill = self.CELL_COLOR
+    def get_previos_output_length(self, sheet_name: str) -> int:
+        if os.path.exists(self.output_path):
+            excel_df = pd.read_excel(self.output_path, sheet_name=sheet_name)
+            print(excel_df)
+            return len(excel_df['description'])
+        return 0
 
+    def write_data_to_one_excel_file(self) -> None:
+        month = input('Podaj miesiac, ktorego dotyczy wyciag: ')
+        for csv_file in os.listdir(self.csv_dir):
+            csv_filename = csv_file.removesuffix('.csv')
+            csv_df = pd.read_csv(f'{self.csv_dir}/{csv_file}', sep=';')
+
+            previous_data_length = self.get_previos_output_length(csv_filename)
+
+            with pd.ExcelWriter(self.output_path, engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
+                if csv_filename in writer.book.sheetnames:
+                    sheet = writer.book[csv_filename]
+                else:
+                    sheet = writer.book.create_sheet(csv_filename)
+
+                sheet.cell(previous_data_length + 2, 4, month)
+                sheet.cell(previous_data_length + 2, 4).fill = self.CELL_COLOR
+                
+                current_data_length = previous_data_length + len(csv_df['description']) + 2
+                sheet.cell(2, 4, f'=SUM(B2:B{current_data_length})')
+                csv_df.to_excel(writer, sheet_name=csv_filename, index=False, header=False, startrow=previous_data_length+2)
+        
                 
 
 
 
 if __name__ == '__main__':
-    filepath = input('filename: ')
-    test = ExcelHandler(filepath)
-    #test.get_data_to_csv_files()
+    #filepath = input('filename: ')
+    test = ExcelHandler()
+    test.get_data_to_csv_files()
+    test.create_excel_file()
     test.write_data_to_one_excel_file()
