ddb204dc0af9f4f08f0c1c4c128b7d6b767f84ae
F4s0lix <janwawrzyniakszkola@gmail.com>
2024-04-09 21:09:06 +0200

    poprawki
    dodano biblioteke tqdm do sledzenia postepu
    poprawiono wiekszosc bledow i TODO od ostatniego commita

--- excel.py
+++ excel.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 import shutil
 from openpyxl.styles import PatternFill, Border, Side
+from tqdm import tqdm
 
 class ExcelHandler:
     def __init__(self, excel_filepath: str, output_path: str = 'output.xlsx') -> None:
@@ -35,21 +36,19 @@ class ExcelHandler:
         PJO.remove('nie kopiować')
         PJO_DICT = {j: {} for j in PJO}
 
-        #TODO: tqdm i sledzenie postepu
         print('\tTworze zestaw rozchodu i dochodu...')
-        for _, row in DATA.iterrows():
+        for _, row in tqdm(DATA.iterrows(), desc='\tPrzetwarzam...', ncols=len(PJO), unit=' kolumn'):
             income = row[column_income]
             expenditure = row[column_expenditure]
             
             if row[column_pjo] in PJO:
                 PJO_DICT[row[column_pjo]][row[column_desc]] = (income, expenditure)
         
-        #TODO: tqdm i sledzenie postepu
         print(f'\tTworze osobne pliki pomocnicze dla: {", ".join(PJO)}')
-        for p in PJO:
+        for p in tqdm(PJO, desc='\tPliki pomocnicze: ', unit=' jednostek'):
             try:
                 with open(f'{self.csv_dir}/{p}.csv', 'a') as file:
-                    file.write('description;income;expenditure\n') #TODO: zmienic na polskie
+                    file.write('opis;przychod;rozchod\n') 
                     for desc, money in PJO_DICT[p].items():
                         income, expend = money
                         file.write(f'{desc};{income:.2f};{expend:.2f}\n')
@@ -67,18 +66,17 @@ class ExcelHandler:
             print(f'\tTworze plik {self.output_path}...')
             with pd.ExcelWriter(self.output_path, engine='openpyxl') as writer:
                 stan_form = '='
-                #TODO: tqdm i sledzenie postepu
-                for file in os.listdir(self.csv_dir):
+                csv_dir_data = os.listdir(self.csv_dir)
+                for file in tqdm(csv_dir_data, desc='\tTworze plik glowny: ', unit=' jednostek'):
                     filename = file.removesuffix('.csv')
-                    print(f'Tworze arkusz dla {filename}')
                     stan_form += f"'{filename}'.D2+"
                     data = pd.read_csv(f'{self.csv_dir}{file}', sep=';')
                     data.to_excel(writer, sheet_name=filename, index=False)
                     sheet = writer.sheets[filename]
-                    sheet.cell(1, 4, 'STAN') # description of value below
+                    sheet.cell(1, 4, 'STAN') # opis formuly
                     sheet.cell(1, 4).fill = self.CELL_COLOR
                     sheet.cell(1, 4).border = self.BORDER_STYLE
-                    end_cell = len(data['description']) + 2 #TODO: zmienic na polskie
+                    end_cell = len(data['opis']) + 2 
                     sheet.cell(2, 4, f'=SUM(B2:B{end_cell})-SUM(C2:C{end_cell})')
                     sheet.cell(2, 4).fill = self.CELL_COLOR
                 print('Tworzenie arkuszy ukonczone pomyslnie.\nTworze arkusz STAN...')
@@ -88,8 +86,9 @@ class ExcelHandler:
                 #stan_form = stan_form.removesuffix('+')
                 sheet.cell(1, 2, stan_form)
                 sheet.cell(1, 3, 'jezeli nie dziala - usunac i dodac jakikolwiek znak, zeby sie przeformatowalo')#FIXME
+                print('Pomyslnie ukonczono tworzenie pliku.')
         except Exception as e:
-            print(f'Nie mozna stworzyc pliku {self.output_path}.\nBlad: {e}')
+            raise ValueError(f'Nie mozna stworzyc pliku {self.output_path}.\nBlad: {e}')
 
     def _get_previos_output_length(self, sheet_name: str) -> int:
         """
@@ -99,12 +98,13 @@ class ExcelHandler:
         """
         print(f'Pobieram poprzednia dlugosc arkusza {sheet_name}')
         try:
-            if os.path.exists(self.output_path): #FIXME: jezeli arkusza nie ma w pliku
-                excel_df = pd.read_excel(self.output_path, sheet_name=sheet_name)
-                #print(excel_df)
-                return len(excel_df['description']) #TODO: zmienic na polskie
+            with pd.ExcelFile(self.output_path, 'openpyxl',) as file:
+                if sheet_name in file.sheet_names: 
+                    excel_df = pd.read_excel(file, sheet_name=sheet_name)
+                    #print(excel_df)
+                    return len(excel_df['opis'])
         except Exception as e:
-            print(f'Nie udalo sie odczytac poprzedniej dlogosci arkusza.\nBlad: {e}')
+            raise ValueError(f'Nie udalo sie odczytac poprzedniej dlogosci arkusza.\nBlad: {e}')
         return 0
 
     def rewrite_STAN_formula(self) -> None:
@@ -120,17 +120,21 @@ class ExcelHandler:
                 for sheet in list_with_sheet_names:
                     stan_form += f"+'{sheet}'.D2"
                 writer.sheets['STAN'].cell(1, 2, stan_form)
+            print('Poprawnie utworzono nowa formule.')
         except Exception as e:
             print(f'Nie udalo sie stworzyc nowej formuly.\nBlad:{e}')
 
 
     def write_data_to_one_excel_file(self) -> None:
         """
-            funkcja dopisuje kolejny miesiac do aktualnego pliku z ewidencj
+            funkcja dopisuje kolejny miesiac do aktualnego pliku z ewidencji
         """
-        # TODO: zrob sprawdzanie bledow
+        self._get_data_to_csv_files()
         month = input('Podaj miesiac, ktorego dotyczy wyciag: ')
-        for csv_file in os.listdir(self.csv_dir):
+        csv_dir_data = os.listdir(self.csv_dir)
+        if len(csv_dir_data) == 0:
+            raise FileNotFoundError('Nie ma plikow do przetowrzenia')
+        for csv_file in tqdm(csv_dir_data, desc='Przetwarzam arkusze: ', unit=' arkusze'):
             csv_filename = csv_file.removesuffix('.csv')
             csv_df = pd.read_csv(f'{self.csv_dir}/{csv_file}', sep=';')
 
@@ -145,14 +149,14 @@ class ExcelHandler:
                 sheet.cell(previous_data_length + 2, 4, month)
                 sheet.cell(previous_data_length + 2, 4).fill = self.CELL_COLOR
                 
-                current_data_length = previous_data_length + len(csv_df['description']) + 2
+                current_data_length = previous_data_length + len(csv_df['opis']) + 2
                 sheet.cell(2, 4, f'=SUM(B2:B{current_data_length})')
                 csv_df.to_excel(writer, sheet_name=csv_filename, index=False, header=False, startrow=previous_data_length+2)
         
 if __name__ == '__main__':
     #filepath = input('filename: ')
-    test = ExcelHandler('dane/03_27_2024_10_52_07-RB 2024 01 (3).xlsx')
+    test = ExcelHandler('../dane-ewidencja/03_27_2024_10_52_07-RB 2024 01 (3).xlsx')
     #test.get_data_to_csv_files()
     test.create_excel_file()
+    test.write_data_to_one_excel_file()
     test.rewrite_STAN_formula()
-    #test.write_data_to_one_excel_file()
